6093a3a4749139007151d4628382349211904708
cypherhades <112914234+0xblur@users.noreply.github.com>
2023-06-29 23:01:50 -0500

    First crash
    I was trying to add components from the LazyVim config and it is broken because it requires many modules imported from its original configuration. Copy-pasting the snippets from the guide is becoming problematic. I'm trying to fix this UI issues replicating and following the tracebacks.

--- lua/base/ui.lua
+++ lua/base/ui.lua
@@ -1,4 +1,5 @@
 return {
+
   {
     "nvim-tree/nvim-web-devicons",
     dependencies = { "DaikyXendo/nvim-material-icon" },
@@ -10,14 +11,14 @@ return {
   },
   -- Better vim.notify()
   {
-    "rcarriaga/nvim-notify",
+    "rcarriga/nvim-notify",
     keys = {
       {
         "<leader>un",
-	function()
-	  require("notify").dismiss({ silent = true, pending = true })
-	end,
-	desc = "Dismiss all notifications",
+        function()
+          require("notify").dismiss { silent = true, pending = true }
+        end,
+        desc = "Dismiss all notifications",
       },
     },
     opts = {
@@ -28,17 +29,140 @@ return {
       max_width = function()
         return math.floor(vim.o.columns * 0.75)
       end,
+    },
+    init = function()
+      -- when noice is not enabled, install notify on VeryLazy
+      local Util = require "base.util"
+      if not Util.has "noice.nvim" then
+        Util.on_very_lazy(function()
+          vim.notify = require "notify"
+        end)
+      end
+    end,
   },
-  init = function()
-    -- when noice is not enabled, install notify on VeryLazy
-    local Util = require("lazyvim.util")
-    if not Util.has("noice.nvim") then
-      Util.on_very_lazy(function()
-        vim.notify = require("notify")
-      end)
-    end
-  end,
-}  },
+
+  -- Noicer UI
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            any = {
+              { find = "%d+L, %d+B" },
+              { find = "; after #%d+" },
+              { find = "; before #%d+" },
+            },
+          },
+          view = "mini",
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = true,
+      },
+    },
+    -- stylua: ignore
+    keys = {
+      { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
+      { "<leader>snl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
+      { "<leader>snh", function() require("noice").cmd("history") end, desc = "Noice History" },
+      { "<leader>sna", function() require("noice").cmd("all") end, desc = "Noice All" },
+      { "<leader>snd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
+      { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll forward", mode = {"i", "n", "s"} },
+      { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll backward", mode = {"i", "n", "s"}},
+    },
+  },
+
+  -- Lualine
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function()
+      local icons = require("config").icons
+      local Util = require "base.util"
+      return {
+        options = {
+          theme = "auto",
+          globalstatus = true,
+          disabled_filetypes = { statusline = { "dashboard", "alpha" } },
+        },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch" },
+          lualine_c = {
+            {
+              "diagnostics",
+              symbols = {
+                error = icons.diagnostics.Error,
+                warn = icons.diagnostics.Warn,
+                info = icons.diagnostics.Info,
+                hint = icons.diagnostics.Hint,
+              },
+            },
+            { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+            { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
+            -- stylua: ignore
+            {
+              function() return require("nvim-navic").get_location() end,
+              cond = function() return package.loaded["nvim-navic"] and require("nvim-navic").is_available() end,
+            },
+          },
+          lualine_x = {
+            -- stylua: ignore
+            {
+              function() return require("noice").api.status.command.get() end,
+              cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+              color = Util.fg("Statement"),
+            },
+            -- stylua: ignore
+            {
+              function() return require("noice").api.status.mode.get() end,
+              cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+              color = Util.fg("Constant"),
+            },
+            -- stylua: ignore
+            {
+              function() return "  " .. require("dap").status() end,
+              cond = function () return package.loaded["dap"] and require("dap").status() ~= "" end,
+              color = Util.fg("Debug"),
+            },
+            { require("lazy.status").updates, cond = require("lazy.status").has_updates, color = Util.fg "Special" },
+            {
+              "diff",
+              symbols = {
+                added = icons.git.added,
+                modified = icons.git.modified,
+                removed = icons.git.removed,
+              },
+            },
+          },
+          lualine_y = {
+            { "progress", separator = " ", padding = { left = 1, right = 0 } },
+            { "location", padding = { left = 0, right = 1 } },
+          },
+          lualine_z = {
+            function()
+              return " " .. os.date "%R"
+            end,
+          },
+        },
+        extensions = { "neo-tree", "lazy" },
+      }
+    end,
+  },
+
   {
     "folke/tokyonight.nvim",
     lazy = false,
---
+++ lua/base/util/init.lua
@@ -0,0 +1,7 @@
+local Util = require "lazy.core.util"
+
+local M = {}
+
+M.root_patterns = { ".git", "lua" }
+
+return M
